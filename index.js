3c8e326945b92e84cf74899c9cea76d69ba534d1
DrFariz <nurrohmna129@gmail.com>
2025-07-04 20:40:19 +0800

    First-Push

---
+++ index.js
@@ -0,0 +1,43 @@
+const puppeteer = require("puppeteer");
+const path = require("path");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+
+  try {
+    await page.goto("https://pvsz2.ru/editor/en", {
+      waitUntil: "domcontentloaded",
+      timeout: 60_000,
+    });
+
+    const fileInput = await page.$('input[name="f-editor-rton"]');
+    const submitFile = await page.$('button[name="f-editor-upload"]');
+
+    await fileInput.uploadFile(path.resolve(__dirname, "pp.dat"));
+
+    await page.evaluate(() => {
+      document
+        .querySelector('input[name="f-editor-rton"]')
+        .dispatchEvent(new Event("change", { bubbles: true }));
+    });
+
+    await delay(232);
+    await page.screenshot({ path: "./result/edi.png", fullPage: true });
+    await submitFile.click();
+    await delay(500);
+
+    console.log(page.url());
+    await page.screenshot({ path: "./result/editor.png" });
+  } catch (err) {
+    console.error("Error:", err.message);
+  } finally {
+    await browser.close();
+  }
+})();
+
+async function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
